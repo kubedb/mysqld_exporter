aa01aa8a4ca39347f16c656eeebeb5e1ea5ce040
Md. Alif Biswas <alif@appscode.com>
2021-11-17 13:40:25 +0600

    Add Custom Queries (#1)
    Signed-off-by: Alif Biswas <alif@appscode.com>

--- mysqld_exporter.go
+++ mysqld_exporter.go
@@ -117,6 +117,7 @@ var scrapers = map[collector.Scraper]bool{
 	collector.ScrapeSlaveHosts{}:                          false,
 	collector.ScrapeReplicaHost{}:                         false,
 	collector.ScrapeRocksDBPerfContext{}:                  false,
+	collector.ScrapeCustomQuery{}:                         false,
 }
 
 func filterScrapers(scrapers []collector.Scraper, collectParams []string) []collector.Scraper {
